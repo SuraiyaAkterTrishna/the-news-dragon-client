e839a4637a1ec68433f996dc2b6217e684ad6624
SuraiyaAkterTrishna <suraiyaaktertrishna@gmail.com>
2023-12-24 10:44:16 +0600

    category news data load from server done

--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -1,11 +1,13 @@
-import { useParams } from "react-router-dom";
+/* eslint-disable no-unused-vars */
+import { useLoaderData, useParams } from "react-router-dom";
 
 
 const Category = () => {
     const {id} = useParams();
+    const categoryNews = useLoaderData();
     return (
         <div>
-            <h1>This is category: {id}</h1>
+            <h1>This Category News: {categoryNews.length}</h1>
         </div>
     );
 };
