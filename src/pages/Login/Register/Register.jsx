085ac1286c1e819aabda0ab08b238f154765ef8e
Kamrujjaman Rony <kamrujjaman0228@gmail.com>
2023-12-29 11:19:41 +0600

    register form UI design

--- src/pages/Login/Register/Register.jsx
+++ src/pages/Login/Register/Register.jsx
@@ -1,10 +1,73 @@
+import { Button, Container, Form } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Register = () => {
-    return (
-        <div>
-            <h2>This is register</h2>
-        </div>
-    );
+  return (
+    <Container
+      className="w-25 mx-auto mt-5 p-5"
+      style={{ backgroundColor: "#f3f2f2" }}
+    >
+      <h3>Register Your Account</h3>
+      <hr />
+      <Form>
+        <Form.Group className="mb-3 fw-bold" controlId="formBasicName">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            className="rounded-0 border-0"
+            style={{ backgroundColor: "rgb(227, 223, 223)" }}
+            type="text"
+            name="name"
+            placeholder="Your Name"
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3 fw-bold" controlId="formBasicPhotoUrl">
+          <Form.Label>Photo URL</Form.Label>
+          <Form.Control
+            className="rounded-0 border-0"
+            style={{ backgroundColor: "rgb(227, 223, 223)" }}
+            type="text"
+            name="photo"
+            placeholder="Photo URL"
+            required
+          />
+        </Form.Group>
+        <Form.Group className="mb-3 fw-bold" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            className="rounded-0 border-0"
+            style={{ backgroundColor: "rgb(227, 223, 223)" }}
+            type="email"
+            name="email"
+            placeholder="Enter Your Email Address"
+            required
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3 fw-bold" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            className="rounded-0 border-0"
+            style={{ backgroundColor: "rgb(227, 223, 223)" }}
+            type="password"
+            name="password"
+            placeholder="Enter Your Password"
+            required
+          />
+        </Form.Group>
+
+        <Button variant="dark" className="rounded-0 w-100 mt-2">
+          Login
+        </Button>
+        <br />
+        <Form.Text className="text-secondary">
+          Already have an account? <Link to="/login">Please Login</Link>
+        </Form.Text>
+        <Form.Text className="text-success"></Form.Text>
+        <Form.Text className="text-danger"></Form.Text>
+      </Form>
+    </Container>
+  );
 };
 
 export default Register;
